b7ad0dc20d13615876ff3124e7f76777b5270a9e
Jamie Stowell <74518646+jamie-prog@users.noreply.github.com>
2022-11-10 14:38:18 -0500

    Update basic params

--- basic.php
+++ basic.php
@@ -1,15 +1,15 @@
 <?php
-define("APPKEY", "167abb701f1d34b021dd1fc2098a82e0");
-define("APPSECRET", "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");
-define("DB_server", "localhost");
-define("DB_name", "brianflo_qtybreak");
-define("DB_user", "getcreat_qtybreak_fox");
-define("DB_pass", "J!}SsF57C=C4");
-define("STORES_TABLE", "RegisteredStores");
-define("SCHEMES_TABLE", "schemes");
-define("TIERES_TABLE", "tieres");
-define("PRODUCTS_TABLE", "Products");
-define("ROOT_PATH", "/qtybreakapp-no-oop/");
+define("APPKEY", "*****");
+define("APPSECRET", "*****");
+define("DB_server", "****");
+define("DB_name", "******");
+define("DB_user", "******");
+define("DB_pass", "*****");
+define("STORES_TABLE", "******");
+define("SCHEMES_TABLE", "******");
+define("TIERES_TABLE", "*****");
+define("PRODUCTS_TABLE", "******");
+define("ROOT_PATH", "**************");
 define("BASICMODULEPATH", ROOT_PATH."/basic-modules");
 define("API_VERSION", "2022-04");
 
